c0acd156584980a38be9a5476be6b1848b26fc02
sanjeevkumar00 <krsanjeev.567@gmail.com>
2023-11-25 15:54:47 +0530

    start project

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const route = require("./routes/route")
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://rizwan786developer:rizwan786@cluster0.raugz1h.mongodb.net/BlogingSite",{
+    useNewUrlParser: true
+}).then(()=> console.log("MongoDB is Connected"))
+.catch(error => console.log("Error in MongoDB url",error))
+
+// app.use("/api",route)
+const port = 3000
+app.listen(port, function(){
+    console.log(`server is running on port ${port}`)
+})
